fdd5891bb44c015e0535311bf89158988273dec8
Erukala27 <162492722+Erukala27@users.noreply.github.com>
2025-02-18 22:13:21 -0600

    Add files via upload

---
+++ Shapes.py
@@ -0,0 +1,26 @@
+import random
+from turtle import Turtle, Screen
+
+tim = Turtle()
+
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+
+def draw_shape(num_sides):
+    angle =  360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side in range(3, 11):
+    tim.color(random.choice(colours))
+    draw_shape(shape_side)
+
+
+
+
+screen = Screen()
+screen.exitonclick()
+
+
+
